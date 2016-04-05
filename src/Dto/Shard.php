06cc11cfc74115a56247da1a6360a9b197cebdb1
fullmetal248 <fullmetal248@mctk.net>
2016-04-05 16:58:47 +0900

    add ShardDto

---
+++ src/Dto/Shard.php
@@ -0,0 +1,61 @@
+<?php
+namespace RiotAPILib\Dto;
+
+use RiotAPILib\Dto\Collection\DtoArrayList;
+
+class Shard {
+	private $hostName;
+	private $locales;
+	private $name, $regionTag, $slug;
+
+	/**
+	 * Shard constructor.
+	 * @param $hostName
+	 * @param $locales
+	 * @param $name
+	 * @param $regionTag
+	 * @param $slug
+	 */
+	public function __construct($hostName, $locales, $name, $regionTag, $slug) {
+		$this->hostName = $hostName;
+		$this->locales = $locales;
+		$this->name = $name;
+		$this->regionTag = $regionTag;
+		$this->slug = $slug;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getHostName() {
+		return $this->hostName;
+	}
+
+	/**
+	 * @return DtoArrayList
+	 */
+	public function getLocales() {
+		return $this->locales;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName() {
+		return $this->name;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getRegionTag() {
+		return $this->regionTag;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getSlug() {
+		return $this->slug;
+	}
+}
