8e39669ff007d590b8072e9a35e88800d1dce805
fullmetal248 <fullmetal248@mctk.net>
2016-04-05 14:43:27 +0900

    fix "namespace" name

--- src/Dto/ChampionList.php
+++ src/Dto/ChampionList.php
@@ -1,6 +1,7 @@
 <?php
-
 namespace RiotAPILib\Dto;
+
+use RiotAPILib\Dto\Collection\DtoArrayList;
 
 class ChampionList extends DtoArrayList {
 }
--- src/Dto/ChampionMasteryList.php
+++ src/Dto/ChampionMasteryList.php
@@ -1,6 +1,8 @@
 <?php
 namespace RiotAPILib\Dto;
 
+use RiotAPILib\Dto\Collection\DtoArrayList;
+
 class ChampionMasteryList extends DtoArrayList {
 
 }
