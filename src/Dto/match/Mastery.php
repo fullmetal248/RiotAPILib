796bdf78785cf43f305f71481bb621d6659bd652
fullmetal248 <fullmetal248@mctk.net>
2016-04-06 03:37:17 +0900

    Add MasteryDto

---
+++ src/Dto/match/Mastery.php
@@ -0,0 +1,31 @@
+<?php
+namespace RiotAPILib\Dto\current_game\match;
+
+class Mastery {
+	private $masteryId;
+	private $rank;
+
+	/**
+	 * Mastery constructor.
+	 * @param $masteryId
+	 * @param $rank
+	 */
+	public function __construct($masteryId, $rank) {
+		$this->masteryId = $masteryId;
+		$this->rank = $rank;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getRank() {
+		return $this->rank;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getMasteryId() {
+		return $this->masteryId;
+	}
+}
