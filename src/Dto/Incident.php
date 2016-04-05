9e4270450c8db3bea7b4e5f718211e5c8bcba9bd
fullmetal248 <fullmetal248@mctk.net>
2016-04-05 17:30:29 +0900

    add IncidentDto

---
+++ src/Dto/Incident.php
@@ -0,0 +1,54 @@
+<?php
+namespace RiotAPILib\Dto;
+
+use RiotAPILib\Dto\Collection\DtoArrayList;
+
+class Incident {
+	private $isActive;
+	private $createdAt;
+	private $id;
+	private $updates;
+
+	/**
+	 * Incident constructor.
+	 * @param $isActive
+	 * @param $createdAt
+	 * @param $id
+	 * @param $updates
+	 */
+	public function __construct($isActive, $createdAt, $id, $updates) {
+		$this->isActive = $isActive;
+		$this->createdAt = $createdAt;
+		$this->id = $id;
+		$this->updates = $updates;
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function getIsActive() {
+		return $this->isActive;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getCreatedAt() {
+		return $this->createdAt;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getId() {
+		return $this->id;
+	}
+
+	/**
+	 * @return DtoArrayList
+	 */
+	public function getUpdates() {
+		return $this->updates;
+	}
+
+}
