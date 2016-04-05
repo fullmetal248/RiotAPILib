f0b921b42ee0a783af9445c21d5157063e98e712
fullmetal248 <fullmetal248@mctk.net>
2016-04-05 17:32:03 +0900

    add ServiceDto

---
+++ src/Dto/Service.php
@@ -0,0 +1,52 @@
+<?php
+namespace RiotAPILib\Dto;
+
+use RiotAPILib\Dto\Collection\DtoArrayList;
+
+class Service {
+	private $incidents;
+	private $name, $slug, $status;
+
+	/**
+	 * Service constructor.
+	 * @param $incidents
+	 * @param $name
+	 * @param $slug
+	 * @param $status
+	 */
+	public function __construct($incidents, $name, $slug, $status) {
+		$this->incidents = $incidents;
+		$this->name = $name;
+		$this->slug = $slug;
+		$this->status = $status;
+	}
+
+	/**
+	 * @return DtoArrayList
+	 */
+	public function getIncidents() {
+		return $this->incidents;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName() {
+		return $this->name;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getSlug() {
+		return $this->slug;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getStatus() {
+		return $this->status;
+	}
+
+}
