fad7092e79a612d69dafda445fdc20494b90449b
fullmetal248 <fullmetal248@mctk.net>
2016-04-05 15:35:46 +0900

    add FeaturedGamsDto

---
+++ src/Dto/FeaturedGames.php
@@ -0,0 +1,34 @@
+<?php
+namespace RiotAPILib\Dto;
+
+use RiotAPILib\Dto\Collection\DtoArrayList;
+
+class FeaturedGames {
+	private $clientRefreshInterval;
+	private $gameList;
+
+	/**
+	 * FeaturedGames constructor.
+	 * @param $clientRefreshInterval
+	 * @param $gameList
+	 */
+	public function __construct($clientRefreshInterval, $gameList) {
+		$this->clientRefreshInterval = $clientRefreshInterval;
+		$this->gameList = $gameList;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getClientRefreshInterval() {
+		return $this->clientRefreshInterval;
+	}
+
+	/**
+	 * @return DtoArrayList
+	 */
+	public function getGameList() {
+		return $this->gameList;
+	}
+
+}
