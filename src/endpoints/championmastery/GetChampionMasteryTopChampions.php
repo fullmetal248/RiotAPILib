09e3f10a54abab95bb33d2e5e970ae906f977d10
fullmetal248 <fullmetal248@mctk.net>
2016-04-14 02:07:48 +0900

    add GetChampionMasteryTopChampions

---
+++ src/endpoints/championmastery/GetChampionMasteryTopChampions.php
@@ -0,0 +1,22 @@
+<?php
+namespace RiotAPILib\endpoints\championmastery;
+
+use RiotAPILib\endpoints\BaseEndPoint;
+use RiotAPILib\EndPointsTable;
+
+class GetChampionMasteryTopChampions extends BaseEndPoint {
+	/**
+	 * GetChampion constructor.
+	 * @param $apiKey
+	 * @param $region
+	 * @param $playerId
+	 */
+	public function __construct($apiKey, $region, $playerId) {
+		parent::__construct($apiKey, $region);
+		$endPoint = EndPointsTable::getInstance()->get($region);
+		$hostUrl = $endPoint->getHostUrl();
+		$platformId = $endPoint->getPlatformId();
+
+		$this->targetUrl = "https://${hostUrl}/championmastery/location/${platformId}/player/${playerId}/topchampions?api_key=${apiKey}";
+	}
+}
