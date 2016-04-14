fd1c9daf9be8336f155d5f12cc1a453684911b3c
fullmetal248 <fullmetal248@mctk.net>
2016-04-15 01:09:50 +0900

    add GetLeagueBySummonerId

---
+++ src/endpoints/league/GetLeagueBySummonerId.php
@@ -0,0 +1,22 @@
+<?php
+namespace RiotAPILib\endpoints\league;
+
+use RiotAPILib\endpoints\BaseEndPoint;
+use RiotAPILib\EndPointsTable;
+
+class GetLeagueBySummonerId extends BaseEndPoint{
+	/**
+	 * GetChampion constructor.
+	 * @param $apiKey
+	 * @param $region
+	 * @param $summonerId
+	 */
+	public function __construct($apiKey, $region, $summonerId) {
+		parent::__construct($apiKey, $region);
+		$endPoint = EndPointsTable::getInstance()->get($region);
+		$hostUrl = $endPoint->getHostUrl();
+		$regionId = $endPoint->getRegionId();
+
+		$this->targetUrl = "https://${hostUrl}/api/lol/${regionId}/v2.5/league/by-summoner/${summonerId}?api_key=${apiKey}";
+	}
+}
