2897d4d75b859fc46fb986506dd040f2af6e4116
fullmetal248 <fullmetal248@mctk.net>
2016-04-15 21:53:31 +0900

    add GetChampionList

---
+++ src/endpoints/lol_static_data/GetChampionList.php
@@ -0,0 +1,20 @@
+<?php
+namespace RiotAPILib\endpoints\lol_static_data;
+
+use RiotAPILib\endpoints\BaseEndPoint;
+use RiotAPILib\EndPointsTable;
+
+class GetChampionList extends BaseEndPoint {
+	/**
+	 * GetChampion constructor.
+	 * @param $apiKey
+	 * @param $region
+	 */
+	public function __construct($apiKey, $region) {
+		parent::__construct($apiKey, $region);
+		$endPoint = EndPointsTable::getInstance()->get(EndPointsTable::GlobalStatic);
+		$hostUrl = $endPoint->getHostUrl();
+
+		$this->targetUrl = "https://${hostUrl}/api/lol/static-data/jp/v1.2/champion?api_key=${apiKey}";
+	}
+}
