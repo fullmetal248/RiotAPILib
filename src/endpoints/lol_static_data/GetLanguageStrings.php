6dd3301883591fcab0b3947f135b3588502cd195
fullmetal248 <fullmetal248@mctk.net>
2016-04-15 22:26:45 +0900

    add GetLanguageStrings

---
+++ src/endpoints/lol_static_data/GetLanguageStrings.php
@@ -0,0 +1,21 @@
+<?php
+namespace RiotAPILib\endpoints\lol_static_data;
+
+use RiotAPILib\endpoints\BaseEndPoint;
+use RiotAPILib\EndPointsTable;
+
+class GetLanguageStrings extends BaseEndPoint {
+	/**
+	 * GetChampion constructor.
+	 * @param $apiKey
+	 * @param $region
+	 */
+	//TODO add optional param
+	public function __construct($apiKey, $region) {
+		parent::__construct($apiKey, $region);
+		$endPoint = EndPointsTable::getInstance()->get(EndPointsTable::GlobalStatic);
+		$hostUrl = $endPoint->getHostUrl();
+
+		$this->targetUrl = "https://${hostUrl}/api/lol/static-data/jp/v1.2/language-strings?api_key=${apiKey}";
+	}
+}
