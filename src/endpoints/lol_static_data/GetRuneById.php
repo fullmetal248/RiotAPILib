7239b7c637e9beafb0611ddb4308b15933817f21
fullmetal248 <fullmetal248@mctk.net>
2016-04-15 23:04:17 +0900

    add GetRuneById

---
+++ src/endpoints/lol_static_data/GetRuneById.php
@@ -0,0 +1,22 @@
+<?php
+namespace RiotAPILib\endpoints\lol_static_data;
+
+use RiotAPILib\endpoints\BaseEndPoint;
+use RiotAPILib\EndPointsTable;
+
+class GetRuneById extends BaseEndPoint {
+	/**
+	 * GetChampion constructor.
+	 * @param $apiKey
+	 * @param $region
+	 * @param $id
+	 */
+	//TODO add optional param
+	public function __construct($apiKey, $region, $id) {
+		parent::__construct($apiKey, $region);
+		$endPoint = EndPointsTable::getInstance()->get(EndPointsTable::GlobalStatic);
+		$hostUrl = $endPoint->getHostUrl();
+
+		$this->targetUrl = "https://${hostUrl}/api/lol/static-data/jp/v1.2/rune/${id}?api_key=${apiKey}";
+	}
+}
